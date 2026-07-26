2b0494803afc60f48e04e6f15c4a4dd3a1e7c66a
Jhun Kenneth Iniego <49866419+JKIniego@users.noreply.github.com>
2026-07-26 22:41:22 +0800

    Setup Tailwind CSS for frontend

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center bg-slate-900">
+      <Text className="text-xl font-bold text-emerald-400">
+        Tailwind is working on SDK 54!
+      </Text>
     </View>
   );
 }
